47d4725dd201c8820f9a2c9d4782814f371d2762
choisaeam <choi.saeam@Gmail.com>
2019-04-08 23:23:04 +0900

    edit menu.c

--- menu.c
+++ menu.c
@@ -5,10 +5,40 @@ int ask_menu(int is_login){
 // 리턴값 : 선택한 메뉴번호
 // 메뉴번호 : 1. Sign up 2. Log in 3. Log out 0. Exit
   int menu;
-  if(is_login==0)
-    printf("Choose menu : 1. Sign up 2. Log in 0. Exit >> ");
-  else
-    printf("Choose menu : 3. Log out 0. Exit >> ");
-  scanf("%d", &menu);
+  char command[30];
+  if(is_login==0){
+    while(1){
+      printf("> ");
+      scanf("%s",command);
+      if(strcmp(command,"join") == 0){
+	menu = 1;
+	break;
+      }
+      else if(strcmp(command,"login") == 0) {
+	menu = 2;
+	break;
+      }
+      else if(strcmp(command,"list") == 0) {
+	menu = 4;
+	break;
+      }
+      else if(strcmp(command,"exit") ==0){
+	menu = 0;
+	break;
+      }
+      else printf("No such command!\n");
+    } 
+  }else{
+    while(1){
+      printf("# ");
+      fgets(command,30,stdin);
+      command[strlen(command)-1] = '\0';
+      if(strcmp(command,"logout") == 0){
+	printf("Bye!");
+	menu = 3;
+	break;
+      }else printf("%s\n",command);
+    }
+  }
   return menu;
 }
