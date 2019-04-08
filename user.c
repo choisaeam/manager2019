4f6ca62cc889d3bbb7f2ffd9b232e55f21c9ad85
choisaeam <choi.saeam@Gmail.com>
2019-04-08 23:24:30 +0900

    edit user.c user.h

--- user.c
+++ user.c
@@ -1,17 +1,40 @@
 #include "user.h"
 
-int load_file(LOGIN* list[], char* filename){
-  int count=0;
-  FILE *datafile = fopen(filename, "r");
-  while(!feof(datafile)){
-    list[count]=(LOGIN*)malloc(sizeof(LOGIN));
-    fscanf(datafile,"%s %s",list[count]->id,list[count]->password);
-    count++;
+void list(LOGIN* list[],int count){
+  int i;
+  printf("User list (id/pass)\n");
+  for(i = 0;i < count;i ++){
+      printf("[%d] %s / %s\n",i+1,list[i]->id,list[i]->password);
   }
-  printf("%d records read!\n",count);\
+}
+int load_file(LOGIN* list[], char* filename){
+  #ifdef DEBUG_MODE
+    printf("DEBUG>> datafile opened! \\n");
+  #endif  
+  int count=0;
+  FILE *datafile;
+  datafile = fopen(filename, "r");
+  int menu;
+  if(datafile == NULL){
+    printf("%s file not exist! make anyway? (Yes 1, No 2) >> ",filename);
+    scanf("%d",&menu);
+    if(menu == 1){
+	return 0;
+    }else return -1;
+  }
+  else{
+    while(!feof(datafile)){
+      list[count]=(LOGIN*)malloc(sizeof(LOGIN));
+      fscanf(datafile,"%s %s",list[count]->id,list[count]->password);
+      count++;
+    }
+    count --;
+    printf("%d records read!\n",count);
+  }
   fclose(datafile);
   return count;
 }
+
 
 void join(LOGIN* list[], int count){
   char id[20], pass[20];
@@ -39,6 +62,7 @@ void join(LOGIN* list[], int count){
   }
 }
 
+
 int login(LOGIN* list[], int count){
   char id[20], pass[20];
   printf("Enter user id >> ");
@@ -53,7 +77,7 @@ int login(LOGIN* list[], int count){
   }
   if(dup!=1){
     printf("No such user!!\n");
-    return -1;
+    return 0;
   }
   else{
     printf("Enter password >> ");
