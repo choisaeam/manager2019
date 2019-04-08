ecf30c4369c5f7f0d64f46412cbe6d998a5a6439
choisaeam <choi.saeam@Gmail.com>
2019-04-08 23:22:37 +0900

    edit main.c

--- main.c
+++ main.c
@@ -10,12 +10,19 @@ int main(int argc, char* argv[]) {
     return 0;
   }
   int count = load_file(userlist, argv[1]);
+  if( count == -1 ){
+    printf("Bye!");
+    return 0;
+  }
   while(1){
-    if(is_login==0) is_login = login(userlist, count);
     menu_id = ask_menu(is_login); // 현재 로그인 되었는지의 여부를 파라미터로 알려야 한다.
-    if(menu_id==1) join(userlist, count);
+    if(menu_id==1) {
+	join(userlist, count);
+	count++;
+    }
     else if (menu_id==2) is_login = login(userlist, count);
     else if (menu_id==3) logout(&is_login);
+    else if (menu_id==4) list(userlist,count);
     else break;
   }
   save_file(userlist, count, argv[1]);
